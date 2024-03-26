7b12ff6be7a26c06207b0a90d94d1f2c0bf38f3d
CA Gaurangkumar Jani <gaurangjani@outlook.com>
2024-03-26 14:35:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryValue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (
+                int i = 0;
+                i < 255; 
+                i++)
+            {
+                Console.WriteLine($"i = {i} " + Convert.ToString(i,2));
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
